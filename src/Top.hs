f587d5e5c9ee1f620e8d4331ba3149152df77f77
Nick Chapman <nic@amadido.co.uk>
2023-02-17 15:21:35 +0000

    parse CSV from file; query uk mps

--- src/Top.hs
+++ src/Top.hs
@@ -1,26 +1,24 @@
 module Top (main) where
 
 import Data.List (intercalate)
+import Par4 (parse,separated,terminated,alts,many,sat,nl,lit)
 
 main :: IO ()
 main = do
-  putStrLn "*qc*"
-  let byGod :: Pred = PredEq (RefValue (VString "God")) (RefField "author")
-  let q :: Query = Project ["title","pages"] (Filter byGod (Lit books))
-  tab <- evalQI q
-  putStr (pretty tab)
-    where
-      books :: Table
-      books = Table [r1,r2]
-      r1,r2 :: Record
-      r1 = Record { schema, fields = [ VString "bible", VString "God", VString "666" ] }
-      r2 = Record { schema, fields = [ VString "The God Delusion", VString "Richard Dawkins", VString "420" ] }
-      schema = [ "title", "author","pages" ]
+  putStrLn "** explore query compilation **"
+  let project sc = ProjectAs sc sc
+  let calledJohn :: Pred = PredEq (RefValue (VString "John")) (RefField "Forename")
+  let q :: Query = project ["Forename","Surname","Party"] (Filter calledJohn (ScanFile "data/mps.csv"))
+  print q
+  res <- evalQI q
+  print res
+  pure ()
+
+----------------------------------------------------------------------
+-- query language
 
 data Query
-  = Lit Table
-  | Load FilePath
-  | Project Schema Query
+  = ScanFile FilePath
   | ProjectAs Schema Schema Query
   | Filter Pred Query
   deriving Show
@@ -31,14 +29,15 @@ data Pred = PredEq Ref Ref | PredNe Ref Ref
 data Ref = RefValue Value | RefField ColName
   deriving Show
 
+----------------------------------------------------------------------
+-- evaluating queries
+
 evalQI :: Query -> IO Table
 evalQI = eval
   where
     eval :: Query -> IO Table
     eval = \case
-      Lit tab -> pure tab
-      Load{} -> undefined
-      Project sc sub -> eval (ProjectAs sc sc sub)
+      ScanFile filename -> loadTableFromCSV filename
       ProjectAs inp out sub -> do
         Table rs <- eval sub
         pure $ Table [ Record { fields = map (select r) inp, schema=out } | r <- rs ]
@@ -57,18 +56,13 @@ evalRef r = \case
   RefField c -> select r c
 
 select :: Record -> ColName -> Value
-select Record{fields,schema} col = the [ v | (c,v) <- zip schema fields, c==col ]
+select Record{fields,schema} col = the who [ v | (c,v) <- zip schema fields, c==col ]
+  where who = (show ("select",col,schema))
 
-pretty :: Table -> String
-pretty tab@(Table rs) = case rs of
-   [] -> "*empty table*"
-   r@Record{schema=sc1}:rs -> do
-     if (not (all (== sc1) [ sc2 | Record {schema=sc2} <- rs ])) then error (show ( "pretty",tab)) else do
-       intercalate "," sc1 ++ "\n"
-         ++ unlines [ intercalate "," (map show fields) | Record {fields} <- r:rs ]
+----------------------------------------------------------------------
+-- values
 
 data Table = Table [Record]
-  deriving Show
 
 data Record = Record { fields :: [Value], schema :: Schema }
   deriving Show
@@ -79,9 +73,66 @@ type ColName = String
 data Value = VString String
   deriving Eq
 
+----------------------------------------------------------------------
+-- displaying  values
+
+instance Show Table where
+  show = pretty
+
 instance Show Value where
   show = \case
     VString s -> show s
 
-the :: Show a => [a] -> a
-the = \case [a] -> a; as -> error (show ("the",as))
+pretty :: Table -> String
+pretty tab@(Table rs) = case rs of
+   [] -> "*empty table*"
+   r@Record{schema=sc1}:rs -> do
+     if (not (all (== sc1) [ sc2 | Record {schema=sc2} <- rs ])) then error (show ( "pretty",tab)) else do
+       intercalate "," sc1 ++ "\n"
+         ++ unlines [ intercalate "," (map show fields) | Record {fields} <- r:rs ]
+
+----------------------------------------------------------------------
+-- parsing tables from CVS
+
+loadTableFromCSV :: FilePath -> IO Table
+loadTableFromCSV filename = parseCVS <$> readFile filename
+
+parseCVS :: String -> Table
+parseCVS = parse table
+  where
+    table = do
+      sc <- separated (lit ',') word; nl
+      vss <- terminated nl record
+      pure $ mkTable sc vss
+
+    record = map VString <$> separated (lit ',') word
+
+    word = alts [quoted,unquoted]
+
+    quoted = do
+      lit '"'
+      xs <- many notDoubleQuoteOrNL
+      lit '"'
+      pure xs
+
+    unquoted = many notCommaOrNL
+
+    notDoubleQuoteOrNL = sat (\case '"' -> False; '\n' -> False; _ -> True)
+    notCommaOrNL = sat (\case ',' -> False; '\n' -> False; _ -> True)
+
+----------------------------------------------------------------------
+-- misc
+
+mkTable :: Schema -> [[Value]] -> Table
+mkTable schema vss = do
+  let n = length schema
+  Table [ Record { fields = checkLength n vs
+                 , schema }
+        | vs <- vss ]
+
+checkLength :: Show a => Int -> [a] -> [a]
+checkLength n xs =
+  if (length xs == n) then xs else error (show ("checkLength: expected",n,"got",length xs,xs))
+
+the :: Show a => String -> [a] -> a
+the who = \case [a] -> a; as -> error (show ("the",who,as))
